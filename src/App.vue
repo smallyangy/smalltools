e701a9d9ab27c707c7dc8bcfdddfb50ffc6fd01f
DESKTOP-KL0E6A7\hjt-zy <315862477@qq.com>
2022-01-19 11:37:51 +0800

    feat: 完成自动同步部署功能

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
         app
         <a-button @click="handleClick">父级按钮</a-button>
         <p>测试一下自动同步部署gitee pages脚本是否生效</p>
+        <p>测试一下自动同步部署gitee pages脚本是否生效</p>
         <p>
             <Test></Test>
         </p>
